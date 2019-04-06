cc6517d6338cf6691f53ac3cd0ffd637d776caac
Antti Ajanki <antti.ajanki@iki.fi>
2019-04-06 18:46:30 +0300

    A script for running all the data preparation steps

---
+++ scripts/prepare_dataset.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Download "vastaukset kirjallisiin kysymyksiin" dataset from
+# avoindata.eduskunta.fi, extract the sentences and split them into
+# train, dev and test datasets.
+#
+# The dataset will be saved in the subdirectroy vkk.
+#
+# Original documents and intermediate formats will be saved in the
+# subdirectory data.
+
+python3 eduskuntavkk/get_vkk.py
+python3 eduskuntavkk/extract_text.py
+python3 eduskuntavkk/cleanup_vkk.py
+python3 eduskuntavkk/fix_metadata.py
+python3 eduskuntavkk/prepare_train_test.py
